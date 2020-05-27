6ae9199f3a7fc8d284ba2fc5b4dca78dd47846a1
Ikuzen <yannis.samba@gmail.com>
2020-05-27 14:50:43 +0200

    adding some input validation

--- ECF-app/src/app/pages/register/register.component.ts
+++ ECF-app/src/app/pages/register/register.component.ts
@@ -11,13 +11,16 @@ export class RegisterComponent implements OnInit {
   username: string;
   password: string;
   email: string;
-  usernameTooltip = "";
+  usernameError = "";
+  passwordError = "";
+  emailError = "";
+
   user: User;
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
-  checkUserName(){
+  checkUserNameValidity(){
     
   }
   submit() {
@@ -26,8 +29,18 @@ export class RegisterComponent implements OnInit {
     if (this.username && this.password && this.email) {
       this.user = { 'username': this.username, 'password': this.password, 'email': this.email }
       this.userService.create(this.user).subscribe((result) => {
-        console.log(result)
-      });
+        console.log('success')
+      },
+      (error) => {
+        console.log(error);
+        if (error.status === 409){
+          this.usernameError = 'There already is a user with the username "test"';
+        }
+        else if(error?.error?.details[0]?.message === '"email" must be a valid email'){
+            this.emailError = '"email" must be a valid email';
+        }
+      })
+      ;
     }
   }
 }
