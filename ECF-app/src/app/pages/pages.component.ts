b72bae924d9cf10168d1529e249ae6be1e174684
Ikuzen <yannis.samba@gmail.com>
2020-05-27 12:05:00 +0200

    add prime ng components, create the login form

--- ECF-app/src/app/pages/pages.component.ts
+++ ECF-app/src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigate(link: any){
+    this.router.navigate([link]);
+  }
 }
